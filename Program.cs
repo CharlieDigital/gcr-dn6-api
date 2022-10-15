91fa05168fc1b4a75bc46a6b04a5f2652acd5079
Charles Chen <chen.charles.c@gmail.com>
2022-10-15 18:55:24 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.Urls.Add("http://0.0.0.0:8080");
+
+app.MapGet("/", () =>
+{
+  return "Hello, World!";
+})
+.WithName("hello");
+
+app.Run();
